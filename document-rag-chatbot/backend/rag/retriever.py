410a9dc5a191c16aa9f5ce27a5441ffd676041b9
ahsaanjameel595 <ahsaanjameel595@gmail.com>
2026-09-20 03:23:06 +0500

    Lazy load RAG retriever

--- document-rag-chatbot/backend/rag/retriever.py
+++ document-rag-chatbot/backend/rag/retriever.py
@@ -2,17 +2,28 @@ from backend.config import TOP_K
 from backend.rag.vectorstore import get_vectorstore
 
 
-vectorstore = get_vectorstore()
+retriever = None
 
-retriever = vectorstore.as_retriever(
-    search_type="similarity",
-    search_kwargs={
-        "k": TOP_K
-    },
-)
+
+def get_retriever():
+    global retriever
+
+    if retriever is None:
+        vectorstore = get_vectorstore()
+
+        retriever = vectorstore.as_retriever(
+            search_type="similarity",
+            search_kwargs={
+                "k": TOP_K
+            },
+        )
+
+    return retriever
 
 
 def retrieve_documents(query: str):
+
+    retriever = get_retriever()
 
     documents = retriever.invoke(query)
 
