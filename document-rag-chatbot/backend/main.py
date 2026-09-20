48aeb48ea759955f9b684350cb758ccd677256db
ahsaanjameel595 <ahsaanjameel595@gmail.com>
2026-09-20 16:37:07 +0500

    Fix CORS for Vercel frontend

--- document-rag-chatbot/backend/main.py
+++ document-rag-chatbot/backend/main.py
@@ -16,7 +16,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+    "http://localhost:5173",
+    "https://document-rag-chatbot-seven.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
